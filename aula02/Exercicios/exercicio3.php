df83bed49721b8e9d133bc1cfb488ef50d4ce42d
Leozaia <161628103+LeonardoZaia22@users.noreply.github.com>
2025-03-04 11:05:12 -0300

    first commit

---
+++ aula02/Exercicios/exercicio3.php
@@ -0,0 +1,19 @@
+<?php
+    $num1 = 70;
+    $num2 = 7;
+
+    $ad = $num1 + $num2;
+    echo "Sua adição resultou em: " . $ad . "<br>";
+
+    $sub = $num1 - $num2;
+    echo "Sua subtração resultou em: " . $sub . "<br>";
+
+    $div = $num1 / $num2;
+    echo "Sua divisão resultou em: " . $div . "<br>";
+
+    $mult = $num1 * $num2;
+    echo "Sua multiplicação resultou em: " . $mult . "<br>";
+
+    $rest = $num1 % $num2;
+    echo "O resto é: " . $rest . "<br>";
+?>
---
+++ aula02/exemplo04.php
@@ -0,0 +1,11 @@
+<?php
+    define('leo', 1.6);
+    $xyz = 1.6;
+    
+
+    echo " O valor é ".leo;
+    echo "<br>";
+    echo "O valor é ".$xyz;
+
+
+?>
